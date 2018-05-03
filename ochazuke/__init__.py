e624555b878f33584c31acbec913ab3bbc5648b1
Karl Dubost <kdubost@mozilla.com>
2018-05-03 14:22:50 +0900

    Creates app (as a factory)

---
+++ ochazuke/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Create Ochazuke: the webcompat-metrics-server Flask application."""
+
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """Create the main webcompat metrics server app."""
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # A route for starting
+    @app.route('/')
+    def index():
+        return 'Welcome to ochazuke'
+
+    return app
