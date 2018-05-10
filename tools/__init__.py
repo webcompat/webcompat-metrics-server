b7693f6a9b8490066910f5c720ce53cd005445eb
Karl Dubost <kdubost@mozilla.com>
2018-05-10 15:29:57 +0900

    Issue #5 - Adds __init__.py in tools and tests

---
+++ tools/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
+
+# Define the application base directory
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+DATA_PATH = os.path.join(BASE_DIR, 'data')
+# Add the data/ directory if it doesn't exist.
+if not os.path.exists(DATA_PATH):
+    os.makedirs(DATA_PATH)
