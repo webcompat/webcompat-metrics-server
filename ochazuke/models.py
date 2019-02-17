78819aab5b8211278dda88b655d79a03ce0572b7
laghee <katrua@gmail.com>
2019-02-17 20:08:35 +0100

    Issue #82 - Create daily new issue table model for db

--- ochazuke/models.py
+++ ochazuke/models.py
@@ -8,3 +8,23 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class DailyTotal(db.Model):
+    """Define a DailyTotal for new issues filed.
+
+    An daily total has:
+
+    * a unique table id
+    * a day representing the date that corresponds to the total
+    * a count of the issues filed on this date
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.DateTime, nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        """Return a representation of a DailyTotal."""
+        return "<DailyTotal for {day}: {count}".format(
+            day=self.day, count=self.count)
