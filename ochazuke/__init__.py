a1a532ca574cfeb36190ce0cec55fa46d9d1c3f3
laghee <katrua@gmail.com>
2019-03-17 21:21:15 +0100

    Issue #95 - Add JSON templates to db test response

--- ochazuke/__init__.py
+++ ochazuke/__init__.py
@@ -118,8 +118,13 @@ def create_app(test_config=None):
                     count=item.count)
                 timeline.append(hourly_count)
             timeline = json.dumps(timeline)
+            response_object = {
+                'about': 'Hourly NeedsDiagnosis issues count',
+                'date_format': 'w3c',
+                'timeline': timeline
+            }
             response = Response(
-                response=timeline,
+                response=response_object,
                 status=200,
                 mimetype='application/json')
         else:
