af1d72113ded838d6a1f6460922191f1317ae3cc
Karl Dubost <kdubost@mozilla.com>
2019-06-04 17:55:29 +0900

    Issue #107 - Creates a specific module for the API
    it also adds tests specifically for the data api

---
+++ ochazuke/api/views.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Set of routes for Ochazuke app."""
+
+import json
+
+from flask import abort
+# from flask import current_app as app
+from flask import request
+from flask import Response
+
+from ochazuke.api import api_blueprint
+from ochazuke.helpers import get_json_slice
+from ochazuke.helpers import get_timeline_data
+from ochazuke.helpers import is_valid_args
+from ochazuke.helpers import is_valid_category
+from ochazuke.helpers import normalize_date_range
+from tools.helpers import get_remote_data
+
+
+
+@api_blueprint.route('/weekly-counts')
+def weekly_reports_data():
+    """Secondhand pipeline for returning weekly JSON data."""
+    json_weekly_data = get_remote_data(
+        'http://laghee.pythonanywhere.com/tmp/weekly_issues')
+    if is_valid_args(request.args):
+        json_weekly_data = get_json_slice(
+            json_weekly_data,
+            request.args.get('from'),
+            request.args.get('to')
+        )
+    response = Response(
+        response=json_weekly_data,
+        status=200,
+        mimetype='application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Vary', 'Origin')
+    return response
+
+
+@api_blueprint.route('/<category>-timeline')
+def issues_count_data(category):
+    """Route for issues count."""
+    if not is_valid_category(category):
+        abort(404)
+    if not request.args:
+        abort(404)
+    if not is_valid_args(request.args):
+        abort(404)
+    # Extract the dates
+    from_date = request.args.get('from')
+    to_date = request.args.get('to')
+    start, end = normalize_date_range(from_date, to_date)
+    # Grab the data
+    timeline = get_timeline_data(category, start, end)
+    # Prepare the response
+    about = 'Hourly {category} issues count'.format(category=category)
+    response_object = {
+        'about': about,
+        'date_format': 'w3c',
+        'timeline': timeline
+    }
+    response = Response(
+        response=json.dumps(response_object),
+        status=200,
+        mimetype='application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Vary', 'Origin')
+    return response
+
+
+@api_blueprint.route('/triage-bugs')
+def triage_bugs():
+    """Returns the list of issues which are currently in triage."""
+    url = 'https://api.github.com/repos/webcompat/web-bugs/issues?sort=created&per_page=100&direction=asc&milestone=2'  # noqa
+    json_data = get_remote_data(url)
+    response = Response(
+        response=json_data,
+        status=200,
+        mimetype='application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Vary', 'Origin')
+    return response
---
+++ tests/unit/test_api.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Main testing module for Webcompat Metrics Server."""
+import json
+import os
+import unittest
+from unittest.mock import patch
+
+from ochazuke import create_app
+from ochazuke import db
+
+
+TIMELINE = {'about': 'Hourly NeedsDiagnosis issues count',
+            'date_format': 'w3c',
+            'timeline': []
+            }
+
+DATA = [{"count": "485", "timestamp": "2018-05-15T01:00:00Z"},
+        {"count": "485", "timestamp": "2018-05-16T02:00:00Z"},
+        {"count": "485", "timestamp": "2018-05-17T03:00:00Z"},
+        {"count": "485", "timestamp": "2018-05-18T04:00:00Z"},
+        ]
+
+DATA2 = [{"count": "485", "timestamp": "2018-05-16T02:00:00Z"},
+         {"count": "485", "timestamp": "2018-05-17T03:00:00Z"},
+         {"count": "485", "timestamp": "2018-05-18T04:00:00Z"},
+         ]
+
+WEEKLY_DATA = {
+    "2015": [9, 7, 46],
+    "2016": [11, 19, 28],
+    "2017": [35, 29, 40]
+}
+
+
+def mocked_json(expected_data):
+    """Prepare a json response when fed a dictionary."""
+    return json.dumps(expected_data)
+
+
+def json_data(filename):
+    """Return a tuple with the content and its signature."""
+    current_root = os.path.realpath(os.curdir)
+    fixtures_path = 'tests/fixtures'
+    path = os.path.join(current_root, fixtures_path, filename)
+    with open(path, 'r') as f:
+        json_event = json.dumps(json.load(f))
+    return json_event
+
+
+class APITestCase(unittest.TestCase):
+    """General Test Cases for views."""
+
+    def setUp(self):
+        """Set up tests."""
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        # Initialize a DB?
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    @patch('ochazuke.api.views.get_remote_data')
+    def test_weeklydata(self, mock_get):
+        """Send back on /data/weekly-counts a JSON."""
+        mock_get.return_value = mocked_json(WEEKLY_DATA)
+        rv = self.client.get('/data/weekly-counts')
+        self.assertIn('"2016": [11, 19, 28]', rv.data.decode())
+        self.assertEqual(rv.status_code, 200)
+        self.assertEqual(rv.mimetype, 'application/json')
+        self.assertTrue('Access-Control-Allow-Origin' in rv.headers.keys())
+        self.assertEqual('*', rv.headers['Access-Control-Allow-Origin'])
+        self.assertTrue('Vary' in rv.headers.keys())
+        self.assertEqual('Origin', rv.headers['Vary'])
+        self.assertTrue(
+            'Access-Control-Allow-Credentials' in rv.headers.keys())
+        self.assertEqual('true',
+                         rv.headers['Access-Control-Allow-Credentials'])
+
+    @patch('ochazuke.api.views.get_timeline_data')
+    def test_needsdiagnosis_valid_param(self, mock_timeline):
+        """Valid parameters on /needsdiagnosis-timeline."""
+        mock_timeline.return_value = DATA2
+        url = '/data/needsdiagnosis-timeline?from=2018-05-16&to=2018-05-18'
+        rv = self.client.get(url)
+        self.assertIn(
+            '{"count": "485", "timestamp": "2018-05-17T03:00:00Z"}',
+            rv.data.decode())
+        self.assertIn(
+            '"about": "Hourly needsdiagnosis issues count"',
+            rv.data.decode())
+        self.assertEqual(rv.status_code, 200)
+        self.assertEqual(rv.mimetype, 'application/json')
+        self.assertTrue('Access-Control-Allow-Origin' in rv.headers.keys())
+        self.assertEqual('*', rv.headers['Access-Control-Allow-Origin'])
+        self.assertTrue('Vary' in rv.headers.keys())
+        self.assertEqual('Origin', rv.headers['Vary'])
+        self.assertTrue(
+            'Access-Control-Allow-Credentials' in rv.headers.keys())
+        self.assertEqual('true',
+                         rv.headers['Access-Control-Allow-Credentials'])
+
+    @patch('ochazuke.api.views.get_remote_data')
+    def test_triage_stats(self, mock_get):
+        """/data/triage-bugs sends back JSON."""
+        mock_get.return_value = json_data('triage.json')
+        rv = self.client.get('/data/triage-bugs')
+        self.assertIn(
+            '"title": "example.org - dashboard test"',
+            rv.data.decode())
+        self.assertEqual(rv.status_code, 200)
+        self.assertEqual(rv.mimetype, 'application/json')
+        self.assertTrue('Access-Control-Allow-Origin' in rv.headers.keys())
+        self.assertEqual('*', rv.headers['Access-Control-Allow-Origin'])
+        self.assertTrue('Vary' in rv.headers.keys())
+        self.assertEqual('Origin', rv.headers['Vary'])
+        self.assertTrue(
+            'Access-Control-Allow-Credentials' in rv.headers.keys())
+        self.assertEqual('true',
+                         rv.headers['Access-Control-Allow-Credentials'])
+
+    def test_needsdiagnosis_without_params(self):
+        """/data/needsdiagnosis-timeline without params fail."""
+        rv = self.client.get('/data/needsdiagnosis-timeline')
+        self.assertEqual(rv.status_code, 404)
+
+    def test_needsdiagnosis_invalid_param(self):
+        """Ignore invalid parameters on /needsdiagnosis-timeline."""
+        rv = self.client.get('/data/needsdiagnosis-timeline?blah=foo')
+        self.assertEqual(rv.status_code, 404)
+
+    def test_needsdiagnosis_invalid_param_values(self):
+        """Ignore invalid parameters values on /needsdiagnosis-timeline."""
+        rv = self.client.get('/data/needsdiagnosis-timeline?from=foo&to=bar')
+        self.assertEqual(rv.status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
