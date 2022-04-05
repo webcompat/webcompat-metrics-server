fbce75edd20b1ffea7313de61fe4deff8e714c18
Karl Dubost <karl+github@la-grange.net>
2022-04-05 12:06:29 +0900

    #148. Upgrades modules

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ochazuke',
-    version='0.1.1',
+    version='0.1.2',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
