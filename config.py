4143e17047be12f881e5fa8e7a7dfef8f2b11b1d
Kate Manning <20972610+laghee@users.noreply.github.com>
2022-04-05 18:52:47 +0200

    Addresses (more) complaints about my rustiness

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL")
-
+    
     
 class TestingConfig(Config):
     """Special class for testing purpose"""
